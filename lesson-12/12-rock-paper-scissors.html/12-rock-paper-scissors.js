c6812e277157a7ecc4e9db49c63b45182d4aee1b
el_teo003 <matticiao.gdl@gmail.com>
2025-10-05 18:47:54 +0200

    the rock game aggiornate

--- lesson-12/12-rock-paper-scissors.html/12-rock-paper-scissors.js
+++ lesson-12/12-rock-paper-scissors.html/12-rock-paper-scissors.js
@@ -1,29 +1,118 @@
-let score={
+let score = {
 
     win: 0,
     loss: 0,
-    tie : 0
+    tie: 0
 
 }
 
-document.querySelector(".score").innerHTML = `Wins: ${score.win} Lose: ${score.loss} Tie: ${score.tie}`;
+let isAutoPlay = false;
+let intervalId = 0;
 
 
 
-function changeStatus(n){
+function changeStatus(n) {
 
     const btn = document.querySelector(n);
 
-    btn.classList.toggle("btn-red");
+    // Cambio subito stato
+    btn.classList.add("btn-red");
+    btn.innerText = "RESETTED";
 
-    if(btn.innerText === "RESET"){
 
-        btn.innerText  = "RESETTED";
+    score.win = 0;
+    score.loss = 0;
+    score.tie = 0;
 
-    }else{
+    document.querySelector(".score").innerHTML = "";
+    document.querySelector(".selection").innerHTML = "";
+    document.querySelector(".result").innerHTML = "";
 
-         btn.innerText  = "RESET";
+    setTimeout(() => {
+        btn.classList.remove("btn-red");
+        btn.innerText = "RESET";
+    }, 1000); // 2000 ms = 2 secondi
+}
+
+/* pensiamo a cosa dovrebbe fare il gioco
+  
+    user clicca un bottone ->  la funzione playGame("mossa_dello_user") genera la mossa del computer e genera il risultato della partita
+
+    -> aggiorna il punteggio
+    -> aggiorna l'html
+    -> mostra il risultato della partita
+    -> mostra la mossa del computer
+    -> mostra la mossa dell'user
+
+*/
+
+function playGame(userMove) {
+
+    // genero la mossa del computerMove
+
+    // con la funzione Math.random() genero un numero casuale tra 0 e 1
+
+    // con la scorciatoia dell' if -> confronto le soluzioni e genero il risultato della partita
+
+    const n = Math.random();
+
+    let computerMove = "";
+
+    computerMove = (n < 0.33 ? "rock" : n < 0.66 ? "paper" : "scissors");
+
+    if (userMove === "rock" && computerMove === "scissors" || userMove === "paper" && computerMove === "rock" || userMove === "scissors" && computerMove === "paper") {
+
+        score.win += 1;
+        document.querySelector(".result").innerHTML = `YOU WIN`;
+
+
+    } else if (userMove === "rock" && computerMove === "paper" || userMove === "scissors" && computerMove === "rock" || userMove === "paper" && computerMove === "scissors") {
+
+        score.loss += 1;
+        document.querySelector(".result").innerHTML = `YOU LOSE`;
+
+    } else {
+
+        score.tie += 1;
+        document.querySelector(".result").innerHTML = `NOTHIN TO DO`;
 
     }
 
+    document.querySelector(".score").innerHTML = `Wins: ${score.win} Lose: ${score.loss} Tie: ${score.tie}`;
+    document.querySelector(".selection").innerHTML = `You picked <img class="move-icon" src="../../images/${userMove}-emoji.png"> The pc picked <img class="move-icon" src="../../images/${computerMove}-emoji.png">`;
+
+
 }
+
+
+function autoPlay() {
+    const btn = document.querySelector("#autoplay");
+
+    if (!isAutoPlay) {
+        intervalId = setInterval(() => {
+            let n = Math.random();
+            if (n < 0.33) {
+                playGame("rock");
+            } else if (n < 0.66) {
+                playGame("paper");
+            } else {
+                playGame("scissors");
+            }
+        }, 1000);
+
+        isAutoPlay = true;
+
+        // 🔹 Attivo animazione
+        btn.querySelector(".text").innerText = "PLAYING";
+        btn.classList.add("playing");
+
+    } else {
+        isAutoPlay = false;
+        clearInterval(intervalId);
+
+        // 🔹 Disattivo animazione
+        btn.classList.remove("playing");
+        btn.querySelector(".text").innerText = "AUTOPLAY";
+    }
+}
+
